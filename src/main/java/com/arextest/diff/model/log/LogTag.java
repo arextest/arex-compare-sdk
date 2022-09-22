40a9e74fe08945ecc3306ef2ce3cf0bea9298ec7
coryhh <jschenrui@163.com>
2022-09-22 20:16:53 +0800

    feat:modify the logic of ignore

--- src/main/java/com/arextest/diff/model/log/LogTag.java
+++ src/main/java/com/arextest/diff/model/log/LogTag.java
@@ -4,18 +4,9 @@ import java.io.Serializable;
 
 public class LogTag implements Serializable {
 
-    private Boolean ig = false;
     private int errorType = 0;
 
     public LogTag() {
-    }
-
-    public Boolean getIg() {
-        return ig;
-    }
-
-    public void setIg(Boolean ig) {
-        this.ig = ig;
     }
 
     public int getErrorType() {
