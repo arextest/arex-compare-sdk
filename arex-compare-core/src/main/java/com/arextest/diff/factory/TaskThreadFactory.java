a34155ec8021e4dd1707ef7b0ff589ddabd5d239
rchen9 <rchen9@trip.com>
2023-11-09 16:21:41 +0800

    style: code style

--- arex-compare-core/src/main/java/com/arextest/diff/factory/TaskThreadFactory.java
+++ arex-compare-core/src/main/java/com/arextest/diff/factory/TaskThreadFactory.java
@@ -10,6 +10,7 @@ public class TaskThreadFactory {
   private static final int CORE_SIZE = Runtime.getRuntime().availableProcessors();
   private static final int CORE_POOL_SIZE = 2;
   private static final int MAX_POOL_SIZE = CORE_SIZE + 1;
+  private static final int QUENE_SIZE = 500;
   public static ExecutorService jsonObjectThreadPool = new ThreadPoolExecutor(CORE_POOL_SIZE,
       MAX_POOL_SIZE,
       1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(QUENE_SIZE),
@@ -20,6 +21,6 @@ public class TaskThreadFactory {
       1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(QUENE_SIZE),
       new NamedThreadFactory("structureHandler"),
       new CallerRunsPolicyWithReport("structureHandler"));
-  private static final int QUENE_SIZE = 500;
+
 
 }
