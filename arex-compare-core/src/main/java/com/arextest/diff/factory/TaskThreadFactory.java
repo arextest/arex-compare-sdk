4e0818e3d2309ae85e9ce0d65451dbbe0320c59d
Rui Chen <44330697+coryhh@users.noreply.github.com>
2024-02-26 16:48:52 +0800

    fix: avoid the maximum number of threads being smaller than the number of core threads (#53)

--- arex-compare-core/src/main/java/com/arextest/diff/factory/TaskThreadFactory.java
+++ arex-compare-core/src/main/java/com/arextest/diff/factory/TaskThreadFactory.java
@@ -8,15 +8,14 @@ import java.util.concurrent.TimeUnit;
 public class TaskThreadFactory {
 
   private static final int CORE_SIZE = Runtime.getRuntime().availableProcessors();
-  private static final int CORE_POOL_SIZE = 4;
   private static final int MAX_POOL_SIZE = CORE_SIZE + 1;
   private static final int QUENE_SIZE = 500;
-  public static ExecutorService jsonObjectThreadPool = new ThreadPoolExecutor(CORE_POOL_SIZE,
+  public static ExecutorService jsonObjectThreadPool = new ThreadPoolExecutor(CORE_SIZE,
       MAX_POOL_SIZE,
       1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(QUENE_SIZE),
       new NamedThreadFactory("JsonObject"),
       new CallerRunsPolicyWithReport("JsonObject"));
-  public static ExecutorService structureHandlerThreadPool = new ThreadPoolExecutor(CORE_POOL_SIZE,
+  public static ExecutorService structureHandlerThreadPool = new ThreadPoolExecutor(CORE_SIZE,
       MAX_POOL_SIZE,
       1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(QUENE_SIZE),
       new NamedThreadFactory("structureHandler"),
